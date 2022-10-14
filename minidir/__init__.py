4dbdce289c2d4498a85c09c130d6aa1efa253e06
Weida Hong <wdhongtw@gmail.com>
2022-10-15 02:08:55 +0800

    Use single module pattern

--- minidir/__init__.py
+++
@@ -1,7 +0,0 @@
-"""pydir is a package for minimal interface for directories and files.
-
-The interfaces of Directory and File is defined with typing protocol.
-And two Directory implementation is provided: FakeDirectory and SystemDirectory.
-"""
-
-from .dir import *
